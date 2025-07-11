b81cf83e1b5c8e76dacb69cbb4e0e53f49489440
Nandajuturu770 <nandajuturu@gmail.com>
2025-07-11 12:11:32 +0530

    set cell value function is implemented in the excel handle class

--- Automation/src/handling_excel/HandlingExcel.java
+++ Automation/src/handling_excel/HandlingExcel.java
@@ -1,9 +1,9 @@
 package handling_excel;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Iterator;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -16,11 +16,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class HandlingExcel {
 
 	FileInputStream fis;
+	FileOutputStream fos;
 	Workbook workbook;
 	Sheet sheet;
+	String sheetAddress;
 
 	public HandlingExcel(String sheetAddress, String sheetName) {
 		try {
+			this.sheetAddress = sheetAddress;
 			this.fis = new FileInputStream(sheetAddress);
 			this.workbook = WorkbookFactory.create(fis);
 			this.sheet = workbook.getSheet(sheetName);
@@ -31,7 +34,7 @@ public class HandlingExcel {
 
 	/**
 	 * @description this method is used to get the cell value
-	 * @param row        c
+	 * @param row        <code>int</code>
 	 * @return cellValue <code>String</code>
 	 */
 	public String getCellvalue(int row , int colunm) {
@@ -46,7 +49,7 @@ public class HandlingExcel {
 	public int getLastRowNumber() {
 		return sheet.getLastRowNum();
 	}
-	
+
 	/**
 	 * @description this methos is used to get last column of any row.
 	 * @param row               <code>int</code>
@@ -55,7 +58,25 @@ public class HandlingExcel {
 	public int getLastColounNumber(int row) {
 		return sheet.getRow(row).getLastCellNum();
 	}
-	
+
+	/**
+	 * @description this method is used to set cell value.
+	 * @param row    <code>int</code>
+	 * @param column <code>int</code>
+	 * @param value  <code>String</code>
+	 */
+	public void setCellValue(int row, int column, String value) {
+		sheet.getRow(row).getCell(column).setCellValue(value);
+		try {
+			fos = new FileOutputStream(this.sheetAddress);
+			workbook.write(fos);
+		} catch (Exception exception) {
+			System.out.println("set cell value is not happened, please check below details ::\n"+exception.getMessage());
+		}
+
+
+	}
+
 	/**
 	 * @description this method to used to test login account of actiTime.
 	 * @param username <code>String</code>
@@ -82,6 +103,12 @@ public class HandlingExcel {
 		dr.quit();	
 	}	
 
+	/**
+	 * @descrition this is main method.
+	 * @param args <code>String[]</code>
+	 * @throws EncryptedDocumentException
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws EncryptedDocumentException, IOException {
 		// shet details
 		String sheetAdress = "./data/actiTimeUsers.xlsx";
@@ -93,14 +120,17 @@ public class HandlingExcel {
 			int lastCoulumNumber = handlingExcel.getLastColounNumber(i);
 			for (int j = 0; j < lastCoulumNumber ; j++) {
 				System.out.print(handlingExcel.getCellvalue(i, j) + "  ");
-		    }
+			}
 			System.out.println();
 		}
 		for(int i = 0 ; i < lastRownumber; i++) {
 			System.out.println(handlingExcel.getCellvalue(i,0 ));
 		}
+		handlingExcel.setCellValue(1, 0, "admin");
+		handlingExcel.setCellValue(1, 1, "manager");
 		String username = handlingExcel.getCellvalue(1, 0);
 		String password = handlingExcel.getCellvalue(1, 1);
+		System.out.println(username+"/t"+password);
 		tocheck(username, password);
 	}
 }
